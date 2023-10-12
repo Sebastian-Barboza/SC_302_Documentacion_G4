ece8d54160c5723ae230c0f057ccabba8cbacb26
NicoleGomez12 <147653925+NicoleGomez12@users.noreply.github.com>
2023-10-11 21:40:55 -0600

    Create mi-archivo.js
    Laboratorio

---
+++ mi-archivo.js
@@ -0,0 +1,13 @@
+// Paso A: Imprimir "Hola mundo" en la consola
+console.log("Hola mundo");
+
+// Paso B: Poner un comentario
+const comentario = "Este es un comentario para el archivo";
+console.log(comentario);
+
+// Paso C: Editar el archivo y mostrar un mensaje
+const archivoEditado = "Hola mundo_editado";
+console.log(`Cambios en el archivo: ${archivoEditado}`);
+
+// Commit changes (ficticio en JavaScript)
+console.log("Commit de los cambios");
